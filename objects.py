1fade1feb45518df06b4c75af86ce4076c91ed3b
LukasChristofMeyer <lmeyer66@ivytech.edu>
2024-09-26 21:18:26 -0400

    Added basic Pygame, Card, and Hand functionality.
    Added basic Pygame, Card, and Hand functionality. I don't know exactly how I should have updated objects.py, so I just attached my version to this document. 
    I also added the graphics file, as Pygame needs graphics. 
    All that there is currently is the skeleton of Card enums, viewing hands, and dragging cards. Though I may have made Card and Hand logic by proxy. 
    
    There is a demo in my version of objects.py that you can uncomment if you want to look at it. Just remember to pip install pygame, and run it next to the graphics file!

--- objects.py
+++ objects.py
@@ -147,13 +147,108 @@ class Player:
             self.hasUno = False
 
 
+# Pygame set up
+import pygame
 
+pygame.init()
+
+screenWidth, screenHeight = 800, 600
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("UNO")
+
+
+from enum import Enum
+
+zeroImage = pygame.image.load('graphics/Zero.png')
+
+class CardType(Enum):
+    ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    # Then we have the other types, making this ENUM actually matter.
+    SKIP = 10
+    REVERSE = 11
+    DRAW2 = 12
+    WILD = 13
+    WILD_DRAW_4 = 14
+
+    # Since this is the first time some of you have ever seen ENUMs, and this is a teaching experience... This is not how I usually do ENUMS!
+    # In C++ and Rust, they are usually just simple value types kind of like: 'enum CardType {ONE, TWO, THREE}', and we use them just as a normal variable
+    # But with more object oriented languages, like Java, C#, and I guess Python, enums are classes which can have class functions.
+    # So, to not let an opportunity go to waste, I'm using that class functionality here! We like to use this enums to get images, so I've added functionality here.
+    def getImage(self):
+        match self:
+            case 0: return zeroImage # ZERO = 0. So as we matching self, if self is zero, then viola, we give the image for zero.
+            case _: return zeroImage # If we somehow don't get any of the above cases, we default to zero.
+
+
+wildCardImage = pygame.image.load('graphics/WildCard.png')
+
+class CardColor(Enum):
+    WILDCARD = 0
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+    YELLOW = 4
+
+    def getImage(self):
+        match self:
+            case 0: return wildCardImage
+            case _: return wildCardImage
+
+
+purpleBorder = pygame.image.load('graphics/PurpleBorder.png')
+'''
+Represents a card in UNO.
+'''
+class Card:
+    # These are defined outside of init as part of the class itself, due to init wanting to use a function that uses them 'combineSurface'
+    type = CardType
+    color = CardColor
+
+    def combineSurface(self):
+        # This defines the base surface of the card which we then layer color and type onto. We get the size of a card, and then make sure it has alpha (transparency)
+        self.image = pygame.Surface(wildCardImage.get_size(), pygame.SRCALPHA)
+        self.image.blit(self.color.getImage(), (0,0)) # Put the card color on the surface
+        self.image.blit(self.type.getImage(), (0,0))  # Put the type onto the card color.
+
+    def __init__(self, type : CardType, color : CardColor):
+        self.type = type
+        self.color = color
+        self.selected = False
+
+        self.combineSurface()
+        
+        # This is how we move the card image around. You can set the position via card.rectangle.center(x, y) and a bunch of other functions like .move(x, y)
+        # Then we can send it to be rendered via screen.blit(card.image, card.rectangle)
+        self.rectangle = self.image.get_rect()
+
+    def display(self): 
+        screen.blit(self.image, self.rectangle) # This just adds the card to the current screen. You will have to do screen.flip() to show it. 
+
+    def displayAtCoords(self, x, y): # If you want to set coordinates as you display
+        if x is None:
+            x = self.rectangle.centerx
+        if y is None:
+            y = self.rectangle.centery
+
+        self.rectangle.center = (x, y)
+        self.display()
+
+    def isHovered(self, mouse_pos):
+        if self.rectangle.collidepoint(mouse_pos): # Check if the mouse is over the rectangle
+            self.image.blit(purpleBorder, (0,0)) # We give the card a purple border to show it's being hovered over!
+            return True
+        else:
+            self.combineSurface() # If it's not being hovered over, we reset it, so it no has a purple border.
+            return False
+
+arrow = pygame.image.load('graphics/Arrow.png')
 class Hand:
     '''
     Represents a player's hand in the game.
     '''
     def __init__(self):
         self.cards = []
+        self.firstCard = 0 # This is the left most card, as to be used for rendering. Refers to cards index.
 
     def addCard(self, card):
         '''
@@ -161,16 +256,69 @@ class Hand:
         '''
         self.cards.append(card)
 
+    def lastCard(self): # This is a function since whenever we want the lastCard we should recalculate.
+        cardsPerDisplay = int((screenWidth/64) - 2) # Cards are 48 pixels in width, but we want parts in between. We also want 2 places for the sides. 
+        lastCard = self.firstCard + (cardsPerDisplay - 1) # cardsPerDisplay - 1, because the firstCard counts, and we start at 0.
+        if lastCard > len(self.cards): lastCard = len(self.cards)
+        return (lastCard)
 
-class Card:
-    '''
-    Represents a card in UNO.
-    '''
-    def __init__(self):
-        ...
+    def isOverflowRight(self):
+        cardNum = len(self.cards)
+        if cardNum > self.lastCard(): # If there are more cards then the most right card, we are 'overflowing' to the right
+            return True
+        else:
+            return False
+
+    def isOverflowLeft(self):
+        if 1 < self.firstCard: # If there is any card left of the current cards further left, we are 'overflowing' to the left
+            return True
+        else:
+            return False
 
 
+    def display(self):
+        cardNum = len(self.cards)
 
+        if self.isOverflowRight():
+            screen.blit(arrow, ((screenWidth-72), (screenHeight - 72))) # Without a defined rectangle, blit renders from the top left down.
+
+        if self.isOverflowLeft():
+            screen.blit(pygame.transform.flip(arrow, True, False), (0, screenHeight - 72)) # We flip the arrow and have it now point left.
+
+        currentCard = 1
+        for i in range(self.firstCard, self.lastCard()):
+            card = self.cards[i]
+            if card.selected != True:
+                card.displayAtCoords((currentCard+1) * 64, screenHeight - 36) # We add one so we have room for the left arrow
+            else:
+                card.display()
+            currentCard += 1
+
+    def shiftLeft(self):
+        self.firstCard -= int((screenWidth/64) - 2)
+        if self.firstCard < 0: self.firstCard = 0
+    
+    def shiftRight(self):
+        self.firstCard += int((screenWidth/64) - 2)
+        if self.firstCard >= len(self.cards): self.firstCard = len(self.cards) - 1
+
+''' Current lycommented out, since I decided not to use them just yet. Just hit the left and right arrows to move between cards
+    def rightArrowHovered(self, mouse_pos):
+        if self.isOverflowRight():
+            rightArrow = pygame.Rect((screenWidth-72), 72, 72, 72)
+            if rightArrow.collidepoint(mouse_pos): return True
+            else: return False
+        else: return False
+    
+    def leftArrowHovered(self, mouse_pos):
+        if self.isOverflowRight():
+            leftArrow = pygame.Rect(0, 72, 72, 72)
+            if leftArrow.collidepoint(mouse_pos): return True
+            else: return False
+        else: return False
+'''
+            
+        
 class DiscardPile:
     '''
     Represents the discard pile in UNO.
@@ -227,3 +375,64 @@ class DrawPile:
         '''
         self.cards = discardPile.takeAllButTopCard()
         random.shuffle(self.cards)
+
+
+''' Uncomment this for an example on how the pygame elements work! Remember, you will need pygame, so run pip install pygame in your environment!
+    Also, you will need to have your environment be in the project folder. That's where the images are accessed!
+
+testHand = Hand()
+for _ in range(30):  # We're just adding 30 cards really quickly
+    testCard = Card(CardType.ZERO, CardColor.WILDCARD)
+    testHand.addCard(testCard)
+
+running = True
+while running:
+    mousePos = pygame.mouse.get_pos()
+    clicked = False
+    selected = False
+
+    for event in pygame.event.get(): # This is for external interactions, like movement or keys.
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Just an example of how this would work, for something not like the big X quit button:
+        if event.type == pygame.KEYDOWN: # We check if a key is down overall, to avoid the big elif below.
+            match event.key: # A match statement is just nicer than a bunch of elifs here
+                case pygame.K_LEFT: 
+                    testHand.shiftLeft()
+                case pygame.K_RIGHT:
+                    testHand.shiftRight() 
+        
+    mouse_buttons = pygame.mouse.get_pressed()
+
+    # Set a nice light blue background
+    screen.fill((173, 216, 230))
+
+    for i in range(testHand.firstCard, testHand.lastCard()):
+        card = testHand.cards[i]
+        isHovered = card.isHovered(mousePos)
+        print(selected)
+        print(card.selected)
+        if isHovered == True and mouse_buttons[0] and selected == False: # This is drag and drop, and not selection.
+            card.selected = True
+            selected = True
+            card.rectangle.center = mousePos
+        elif card.selected == True and mouse_buttons[0] and selected == False: # Here, we give it another tick to update, if the mouse moves too fast.
+            card.rectangle.center = mousePos
+            card.selected == False
+            selected = True
+        else:
+            card.selected = False # This is so it's drag and drop, and not selection. Otherwise, it'd be very awkward.
+
+    testHand.display()
+
+    # Update the display
+    pygame.display.flip()
+
+# Clean up
+pygame.quit()
+
+import sys
+sys.exit()
+
+'''
